48c76fb72ea454de27e06094c1d95ffaf8c09179
Thyros <trainzfreak@gmx.de>
2009-07-16 17:51:35 +0200

    - [189] Update ScriptDev2 to 1218.

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1214"
- #define REVISION_DATE "2009-15-07"
-#define REVISION_TIME "22:00"
+ #define REVISION_ID "1218"
+ #define REVISION_DATE "2009-16-07"
+#define REVISION_TIME "14:00"
 #endif // __REVISION_H__
