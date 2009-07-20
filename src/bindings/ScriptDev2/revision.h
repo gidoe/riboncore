ecba5a5ecdda66157cb198c0b80cb42cf1d32ca9
Thyros <trainzfreak@gmx.de>
2009-07-20 07:23:54 +0200

    - [203] Update Scriptdev2 to 1237, and move omp.h to src/tools/openmp Folder.

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1232"
- #define REVISION_DATE "2009-19-07"
-#define REVISION_TIME "17:00"
+ #define REVISION_ID "1237"
+ #define REVISION_DATE "2009-20-07"
+#define REVISION_TIME "07:00"
 #endif // __REVISION_H__
