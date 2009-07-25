2a0a0001eb641d3f25368161ded4d9638c7e8c75
Thyros <Thyros@.(none)>
2009-07-25 16:53:12 +0200

    - [225] Update ScriptDev2 to 1262.

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1258"
+ #define REVISION_ID "1262"
  #define REVISION_DATE "2009-25-07"
-#define REVISION_TIME "08:00"
+#define REVISION_TIME "16:00"
 #endif // __REVISION_H__
