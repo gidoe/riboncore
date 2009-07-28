316a54a98197c04660f08278745b8497a0b47439
Filipper <mtbdriver@gmx.de>
2009-07-28 17:04:35 +0200

    - [251] Enhanced Nexus-Script "boss_ormorok"

---
+++ sql/uecore-fixes/Revision 251/251_creature_template.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET scriptname='boss_ormorok' WHERE entry='26794';
+UPDATE creature_template SET scriptname='mob_crystal_spike' WHERE entry='27099';
+UPDATE creature_template SET scriptname='mob_crystalline_tangler' WHERE entry='32665';
