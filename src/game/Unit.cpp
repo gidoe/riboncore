43685d887f87b7032e4ce83550fdb2cbc2ed5b78
Thyros <Thyros@.(none)>
2009-07-31 23:11:24 +0200

    - [272] Fided the Gold lootbug, and different Loot Bug.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -696,7 +696,7 @@ uint32 Unit::DealDamage(Unit *pVictim, uint32 damage, CleanDamage const* cleanDa
             if(!cVictim->isPet())
             {
                 cVictim->DeleteThreatList();
-                if (cVictim->GetCreatureInfo()->lootid)
+                if (cVictim->GetCreatureInfo()->lootid || cVictim->GetCreatureInfo()->maxgold > 0)
                     cVictim->SetUInt32Value(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
             }
             // Call creature just died function
