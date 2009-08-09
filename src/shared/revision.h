e82ee7a0b909bae06bc789d6ce40a14d3835c5bb
Thyros <Thyros82@gmx.de>
2009-08-09 19:43:59 +0200

    - [304] Added Author to Thanks File.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "303"
+ #define _REVISION "304"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
 #endif // __REVISION_H__
