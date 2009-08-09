3aa363576dc26f350acf468b665091cf45ba0b1c
Thyros <Thyros82@gmx.de>
2009-08-09 17:28:42 +0200

    - Apply new Scripts.

--- src/bindings/Scripts/ScriptMgr.h
+++ src/bindings/Scripts/ScriptMgr.h
@@ -1,11 +1,15 @@
-/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+/* Copyright (C) 2008-2009 Ribon <http://www.dark-resurrection.de/wowsp/>
+ *
+ * Thanks to the original authors: ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
  * This program is free software licensed under GPL version 2
  * Please see the included DOCS/LICENSE.TXT for more information */
 
-#ifndef SC_SCRIPTMGR_H
-#define SC_SCRIPTMGR_H
+#ifndef SCRIPTMGR_H
+#define SCRIPTMGR_H
 
 #include "Common.h"
+#include "Platform/CompilerDefs.h"
 #include "DBCStructure.h"
 
 class Player;
@@ -19,10 +23,11 @@ class SpellCastTargets;
 class Map;
 class Unit;
 class WorldObject;
+struct ItemPrototype;
 
 #define MAX_SCRIPTS         5000                            //72 bytes each (approx 351kb)
 #define VISIBLE_RANGE       (166.0f)                        //MAX visible range (size of grid)
-#define DEFAULT_TEXT        "<ScriptDev2 Text Entry Missing!>"
+#define DEFAULT_TEXT        "<Ribon Script Text Entry Missing!>"
 
 struct Script
 {
@@ -30,7 +35,7 @@ struct Script
         pGossipHello(NULL), pQuestAccept(NULL), pGossipSelect(NULL), pGossipSelectWithCode(NULL),
         pQuestSelect(NULL), pQuestComplete(NULL), pNPCDialogStatus(NULL), pGODialogStatus(NULL),
         pChooseReward(NULL), pItemHello(NULL), pGOHello(NULL), pAreaTrigger(NULL), pItemQuestAccept(NULL),
-        pGOQuestAccept(NULL), pGOChooseReward(NULL), pItemUse(NULL),
+        pGOQuestAccept(NULL), pGOChooseReward(NULL),pItemUse(NULL), pItemExpire(NULL),
         pEffectDummyCreature(NULL), pEffectDummyGameObj(NULL), pEffectDummyItem(NULL),
         GetAI(NULL), GetInstanceData(NULL)
     {}
@@ -39,26 +44,27 @@ struct Script
 
     //Methods to be scripted
     bool (*pGossipHello         )(Player*, Creature*);
-    bool (*pQuestAccept         )(Player*, Creature*, const Quest*);
-    bool (*pGossipSelect        )(Player*, Creature*, uint32, uint32);
-    bool (*pGossipSelectWithCode)(Player*, Creature*, uint32, uint32, const char*);
+    bool (*pQuestAccept         )(Player*, Creature*, Quest const* );
+    bool (*pGossipSelect        )(Player*, Creature*, uint32 , uint32 );
+    bool (*pGossipSelectWithCode)(Player*, Creature*, uint32 , uint32 , const char* );
     bool (*pGOSelect            )(Player*, GameObject*, uint32 , uint32 );
     bool (*pGOSelectWithCode    )(Player*, GameObject*, uint32 , uint32 , const char* );
-    bool (*pQuestSelect         )(Player*, Creature*, const Quest*);
-    bool (*pQuestComplete       )(Player*, Creature*, const Quest*);
-    uint32 (*pNPCDialogStatus   )(Player*, Creature*);
-    uint32 (*pGODialogStatus    )(Player*, GameObject*);
-    bool (*pChooseReward        )(Player*, Creature*, const Quest*, uint32);
-    bool (*pItemHello           )(Player*, Item*, const Quest*);
-    bool (*pGOHello             )(Player*, GameObject*);
-    bool (*pAreaTrigger         )(Player*, AreaTriggerEntry*);
-    bool (*pItemQuestAccept     )(Player*, Item*, const Quest*);
-    bool (*pGOQuestAccept       )(Player*, GameObject*, const Quest*);
-    bool (*pGOChooseReward      )(Player*, GameObject*, const Quest*, uint32);
-    bool (*pItemUse             )(Player*, Item*, SpellCastTargets const&);
-    bool (*pEffectDummyCreature )(Unit*, uint32, uint32, Creature*);
-    bool (*pEffectDummyGameObj  )(Unit*, uint32, uint32, GameObject*);
-    bool (*pEffectDummyItem     )(Unit*, uint32, uint32, Item*);
+    bool (*pQuestSelect         )(Player*, Creature*, Quest const* );
+    bool (*pQuestComplete       )(Player*, Creature*, Quest const* );
+    uint32 (*pNPCDialogStatus   )(Player*, Creature* );
+    uint32 (*pGODialogStatus    )(Player*, GameObject * _GO );
+    bool (*pChooseReward        )(Player*, Creature*, Quest const*, uint32 );
+    bool (*pItemHello           )(Player*, Item*, Quest const* );
+    bool (*pGOHello             )(Player*, GameObject* );
+    bool (*pAreaTrigger         )(Player*, AreaTriggerEntry* );
+    bool (*pItemQuestAccept     )(Player*, Item *, Quest const* );
+    bool (*pGOQuestAccept       )(Player*, GameObject*, Quest const* );
+    bool (*pGOChooseReward      )(Player*, GameObject*, Quest const*, uint32 );
+    bool (*pItemUse             )(Player*, Item*, SpellCastTargets const& );
+    bool (*pItemExpire          )(Player*, ItemPrototype const *);
+    bool (*pEffectDummyCreature )(Unit*, uint32, uint32, Creature* );
+    bool (*pEffectDummyGameObj  )(Unit*, uint32, uint32, GameObject* );
+    bool (*pEffectDummyItem     )(Unit*, uint32, uint32, Item* );
 
     CreatureAI* (*GetAI)(Creature*);
     InstanceData* (*GetInstanceData)(Map*);
@@ -75,4 +81,13 @@ void DoScriptText(int32 textEntry, WorldObject* pSource, Unit* target = NULL);
 #define FUNC_PTR(name, callconvention, returntype, parameters)    typedef returntype(callconvention *name)parameters;
 #endif
 
+#ifdef WIN32
+  #define RIBON_DLL_EXPORT extern "C" __declspec(dllexport)
+#elif defined( __GNUC__ )
+#define RIBON_DLL_EXPORT extern "C"
+#else
+#define RIBON_DLL_EXPORT extern "C" export
 #endif
+
+#endif
+
