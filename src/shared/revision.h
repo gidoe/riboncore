ee12e08ac4c0043602927e6558e1af553af1854d
Thyros <Thyros82@gmx.de>
2009-08-09 19:26:56 +0200

    - [302] fix Leotheras the Blind Channeler reset(visual is still bugged).

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "301"
+ #define _REVISION "302"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
 #endif // __REVISION_H__
