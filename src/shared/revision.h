5b44f1a46296d65984ec18adcd99ff1a5f487d5a
Thyros <Thyros82@gmx.de>
2009-08-09 19:23:10 +0200

    - [301] Apply new Readme, thanks to -=Elite=- Ultra™.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "300"
+ #define _REVISION "301"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
 #endif // __REVISION_H__
