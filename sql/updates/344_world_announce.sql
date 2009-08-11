55dccf256b06f022fb72584ccaf067864568b43b
Thyros <Thyros82@gmx.de>
2009-08-11 17:17:05 +0200

    - Added missing sql

---
+++ sql/updates/344_world_announce.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `autobroadcast`;
+CREATE TABLE `autobroadcast` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`text` longtext NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DELETE FROM `ribon_string` WHERE `entry` = 11000;
+INSERT INTO `ribon_string` (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8)
+VALUES (11000, '|cffffff00[|c00077766Autobroadcast|cffffff00]: |cFFF222FF%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
