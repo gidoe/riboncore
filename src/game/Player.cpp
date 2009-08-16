f1098e86a9eb038d3a13e2da2b16d86ef194c6ea
Thyros <Thyros82@gmx.de>
2009-08-16 07:34:48 +0200

    - [482] Clear m_actionsButtons regardless of DB data for other spec when switching specs.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -15286,8 +15286,6 @@ bool Player::isAllowedToLoot(Creature* creature)
 
 void Player::_LoadActions(QueryResult *result)
 {
-    m_actionButtons.clear();
-    
     if(result)
     {
         do
@@ -21885,6 +21883,7 @@ void Player::ActivateSpec(uint8 spec)
     m_usedTalentCount = spentTalents;
     InitTalentForLevel();
 
+    m_actionButtons.clear();
     QueryResult *result = CharacterDatabase.PQuery("SELECT button,action,type FROM character_action WHERE guid = '%u' AND spec = '%u' ORDER BY button", GetGUIDLow(), m_activeSpec);
     if (result)    
     {
@@ -21895,3 +21894,4 @@ void Player::ActivateSpec(uint8 spec)
     SendActionButtons(1);
     SetPower(getPowerType(), 0);
 }
+
--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "481"
+ #define _REVISION "482"
 #endif // __REVISION_H__
