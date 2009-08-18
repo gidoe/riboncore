4ebd29d378c6c9fad5bb91cba8b1c5d87d93c31d
Thyros <Thyros82@gmx.de>
2009-08-18 08:49:20 +0200

    - [564] set the mistiming value to the right Number.

--- sql/characters.sql
+++ sql/characters.sql
@@ -15,28 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-/*
-
---
--- Table structure for table `character_db_version`
---
-
-DROP TABLE IF EXISTS `character_db_version`;
-CREATE TABLE `character_db_version` (
-  `required_8339_02_characters_character_battleground_data` bit(1) default NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
-
---
--- Dumping data for table `character_db_version`
---
-
-LOCK TABLES `character_db_version` WRITE;
-INSERT INTO `character_db_version` VALUES
-(NULL);
-UNLOCK TABLES;
-
-*/
-
 --
 -- Table structure for table `account_data`
 --
