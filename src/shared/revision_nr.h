2daf9b0ac400ae94e42758e1595bdd5d6223849d
Thyros <Thyros82@gmx.de>
2009-09-03 16:32:47 +0200

    - [804] [8444] Prevent spawn gameobjects with invalid displayid by command. Author: VladimirMangos Also fix mangos string for not existed gameobejct entry.

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define _REVISION "803"
+ #define _REVISION "804"
 #endif // __REVISION_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_803_characters_character_db_version"
- #define REVISION_DB_RIBONCORE "required_803_world_db_version"
+ #define REVISION_DB_RIBONCORE "required_804_8444_world_ribon_string"
  #define REVISION_DB_LOGON "required_803_logon_db_version"
 #endif // __REVISION_SQL_H__
