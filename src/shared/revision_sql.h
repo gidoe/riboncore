252436f61e76bebb3b582f5e6c80f663c5b61eb7
Thyros <Thyros82@gmx.de>
2009-09-08 21:46:07 +0200

    - [841] Reverted Change in 837, and add the Right Change for the new table.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_803_characters_character_db_version"
+ #define REVISION_DB_CHARACTERS "required_841_characters_character_account_data"
  #define REVISION_DB_RIBONCORE "required_840_world_scripts"
  #define REVISION_DB_LOGON "required_803_logon_db_version"
 #endif // __REVISION_SQL_H__
