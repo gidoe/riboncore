7484346f6fd099a0e67f912599fa60bd01801a60
Thyros <Thyros82@gmx.de>
2009-09-11 14:42:02 +0200

    - [854] quest 11221 Reports from the Field (Howling Fjord)

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define _REVISION "853"
+ #define _REVISION "854"
 #endif // __REVISION_H__
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_841_characters_character_account_data"
- #define REVISION_DB_RIBONCORE "required_847_world_script_text"
+ #define REVISION_DB_RIBONCORE "required_854_world_scripts"
  #define REVISION_DB_LOGON "required_803_logon_db_version"
 #endif // __REVISION_SQL_H__
