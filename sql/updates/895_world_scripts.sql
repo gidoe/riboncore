f6eac8f4636fabf02bb131da7e5db9c0666c1d9c
Thyros <Thyros82@gmx.de>
2009-09-20 10:48:03 +0200

    - [895] Add ScriptName to GO 185919 and 185921, thanks WarHead

---
+++ sql/updates/895_world_scripts.sql
@@ -0,0 +1,4 @@
+ALTER TABLE db_version CHANGE COLUMN required_894_world_scripts required_895_world_scripts bit;
+
+UPDATE `gameobject_template` SET `ScriptName`='go_fel_crystalforge' WHERE `entry`=185919;
+UPDATE `gameobject_template` SET `ScriptName`='go_bashir_crystalforge' WHERE `entry`=185921;
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_894_characters_game_event"
- #define REVISION_DB_RIBONCORE "required_894_world_scripts"
+ #define REVISION_DB_RIBONCORE "required_895_world_scripts"
  #define REVISION_DB_LOGON "required_875_logon"
 #endif // __REVISION_SQL_H__
