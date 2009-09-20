269ad7a52b9f0dfecda92184b81652c703d84a00
Thyros <Thyros82@gmx.de>
2009-09-20 10:45:24 +0200

    - [894] quest: "Long Forgotten Memories" - quest: "A Pawn on the Eternal Board" - World Event: AQ-War Effort - quest: "Eranikus, Tyrant of the Dream" - Sunken temple -> "Snake Puzzle event" - duskwood boss "Twilight Corrupter" for quest: "The Nightmare's Corruption" - All by kb_z, please tell me if I missed anything, and somebody please review the SQL.. a LOT Of stuff in this needs to go to TDB!

---
+++ sql/updates/894_characters_game_event.sql
@@ -0,0 +1,5 @@
+ALTER TABLE character_db_version CHANGE COLUMN required_864_characters_character_spell 894_characters_game_event bit;
+
+
+DELETE `game_event_condition_save` FROM `game_event_condition_save` WHERE `event_id` in (22,75,76,77);
+DELETE `game_event_save` FROM `game_event_save` WHERE `event_id` in (22,75,76,77);
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_864_characters_character_spell"
- #define REVISION_DB_RIBONCORE "required_893_world_scripts"
+ #define REVISION_DB_CHARACTERS "required_894_characters_game_event"
+ #define REVISION_DB_RIBONCORE "required_894_world_scripts"
  #define REVISION_DB_LOGON "required_875_logon"
 #endif // __REVISION_SQL_H__
