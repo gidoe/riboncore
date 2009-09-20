6a28a5a31590fab9e41b7537fd055823b8f83230
Thyros <Thyros82@gmx.de>
2009-09-20 10:19:37 +0200

    - [892] Wintergrasp: Prevent defenders team to click the relic. Patch by Spp. - Zum'Rah Area Trigger Script, Zum'Rah should become hostile when approached. By totoro. - Judgement of Light PPM based, not 100%. By Drevi. - Fix Deflection Exploit. By manuel, thanks to TheNecromancer and Gyullo. - Correct Wintergrasp Tenacity formulas, by Gyullo. - A Spirit Guide Escort Quest, code from SD2, patch by manuel. - TrullyOne/MeanMachine Waypoint System Restored. Patch by XTElite1.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_864_characters_character_spell"
- #define REVISION_DB_RIBONCORE "required_890_world_scripts"
+ #define REVISION_DB_RIBONCORE "required_892_world_scripts"
  #define REVISION_DB_LOGON "required_875_logon"
 #endif // __REVISION_SQL_H__
