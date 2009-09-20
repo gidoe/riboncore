389cb846a7d864ee87f2768633cf7afdbd246bfe
Thyros <Thyros82@gmx.de>
2009-09-20 09:12:45 +0200

    - [890] Add NPC 16111 to script npc_training_dummy (this creature is not supposed to move around and kill things, it should behave like a normal target dummy)

---
+++ sql/updates/890_world_scripts.sql
@@ -0,0 +1,4 @@
+ALTER TABLE db_version CHANGE COLUMN required_885_world_scripts required_890_world_scripts bit;
+
+
+UPDATE `creature_template` SET `ScriptName`='npc_training_dummy' WHERE `entry`=16111;
