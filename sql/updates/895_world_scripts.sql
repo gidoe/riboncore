733a837923b4622bf1123d4d53f8bea9b13afcd9
Thyros <Thyros82@gmx.de>
2009-09-20 10:50:17 +0200

    - [896] GO - Southfury Moonstone script, from SD2, patch by manuel - Fix Auctionhouse CPU usage during item expire check, by MrSmite

--- sql/updates/895_world_scripts.sql
+++ sql/updates/895_world_scripts.sql
@@ -2,3 +2,6 @@
 
 UPDATE `gameobject_template` SET `ScriptName`='go_fel_crystalforge' WHERE `entry`=185919;
 UPDATE `gameobject_template` SET `ScriptName`='go_bashir_crystalforge' WHERE `entry`=185921;
+
+
+UPDATE `gameobject_template` SET `ScriptName`='go_southfury_moonstone' WHERE `entry`=185566;
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "895"
+ #define REVISION_NR "896"
 #endif // __REVISION_NR_H__
