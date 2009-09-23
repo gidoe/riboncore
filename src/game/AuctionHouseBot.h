7d833fff070ca0df24187ee6cf317efdd0087a4f
chip2oox <xp_master@primacom.net>
2009-09-24 01:24:17 +0200

    - [923] Fix some Linux build problems

--- src/game/AuctionHouseBot.h
+++ src/game/AuctionHouseBot.h
@@ -902,7 +902,7 @@ public:
     {
         switch(Class)
         {
-        case ITEM_CLASS_TRADE_GOODS:
+        case 7:
             DecItemCounts(Quality);
             break;
         default:
@@ -966,7 +966,7 @@ public:
     {
         switch(Class)
         {
-        case ITEM_CLASS_TRADE_GOODS:
+        case 7:
             IncItemCounts(Quality);
             break;
         default:
