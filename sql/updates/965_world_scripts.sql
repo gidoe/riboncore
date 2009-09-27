389b6ee1b07a8e19cc8f34d69237d9b666636231
Thyros <Thyros82@gmx.de>
2009-09-27 23:49:09 +0200

    - [965] *Instead of checking getVictim() to determine whether or not to regen health, check if the threat list is empty *Fix some incorrect/missing scriptname assignments, thanks Drahy

---
+++ sql/updates/965_world_scripts.sql
@@ -0,0 +1,8 @@
+ALTER TABLE db_version CHANGE COLUMN required_962_world_scripts required_965_world_scripts bit;
+
+
+
+UPDATE `creature_template` SET `ScriptName`='mob_slad_ran_viper' WHERE `entry`=29680;
+
+/* Eck the Ferocious */
+UPDATE `creature_template` SET `ScriptName`='boss_eck' WHERE `entry`=29932;
