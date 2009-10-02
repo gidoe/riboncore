ec578d789b1e32d7a4b3786f213f8a8337cf8f4f
Thyros <Thyros82@gmx.de>
2009-10-02 11:54:45 +0200

    - [989] Passive targets under attack by players are not ideal targets for hostile creatures, so avoid them.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11344,7 +11344,7 @@ Unit* Creature::SelectVictim()
     else
         return NULL;
 
-    if(target && (!target->getVictim() || IsFriendlyTo(target->getVictim())) // if victim(1) has a victim(2), only attack victim(1) if we are friendly with victim(2)
+    if(target && _IsTargetAcceptable(target))
     {
         SetInFront(target);
         return target;
@@ -11357,8 +11357,7 @@ Unit* Creature::SelectVictim()
     for(AttackerSet::const_iterator itr = m_attackers.begin(); itr != m_attackers.end(); ++itr)
     {
         if((*itr) && canCreatureAttack(*itr) && (*itr)->GetTypeId() != TYPEID_PLAYER
-          && !((Creature*)(*itr))->HasUnitTypeMask(UNIT_MASK_CONTROLABLE_GUARDIAN)
-          // && (!(*itr)->getVictim() || IsFriendlyTo((*itr)->getVictim()))) // if victim(1) has a victim(2), only attack victim(1) if we are friendly with victim(2)
+          && !((Creature*)(*itr))->HasUnitTypeMask(UNIT_MASK_CONTROLABLE_GUARDIAN))
             return NULL;
     }
 
@@ -11369,7 +11368,7 @@ Unit* Creature::SelectVictim()
     // search nearby enemy before enter evade mode
     if(HasReactState(REACT_AGGRESSIVE))
         if(target = SelectNearestTarget())
-            if(!target->getVictim() || IsFriendlyTo(target->getVictim())) // if victim(1) has a victim(2), only attack victim(1) if we are friendly with victim(2)
+            if(_IsTargetAcceptable(target))
                 return target;
 
     if(m_invisibilityMask)
